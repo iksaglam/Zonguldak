67a14ae7db33dd5a25600acc4bb08e4181e8f898
İsmail K. Sağlam <iksaglam@gmail.com>
2022-02-07 18:35:39 +0300

    Update plotSFS.R

--- Scripts/plotSFS.R
+++ Scripts/plotSFS.R
@@ -14,6 +14,6 @@ pvar<- (1-sfs[1]-sfs[length(sfs)])*100
 #the variable categories of the sfs
 sfs<-norm(sfs[-c(1,length(sfs))]) 
 pdf(F2)
-barplot(sfs,legend=paste("Variability:= ",round(pvar,3),"%"),xlab="number of derived sites",
+barplot(sfs,xlab="number of derived sites",
 names=1:length(sfs),ylab="Proportions",main=F1,col='brown')
 
